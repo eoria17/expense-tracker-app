fa7be4021e2e75ead9016c18e8a4b0e81b5ec7af
s3600111 <s3600111@student.rmit.edu.au>
2021-06-07 09:36:16 +1000

    account balance now updated on transaction deletion

--- controllers/deleteTransaction.go
+++ controllers/deleteTransaction.go
@@ -27,12 +27,37 @@ func (ae AppEngine) DeleteTransaction(w http.ResponseWriter, r *http.Request) {
 	if err != nil{
 		fmt.Println(err)
 	}
+	//userID := session.Values["user"].(models.User).ID
 
 	//get transaction id from url
 	vars := mux.Vars(r)
     transaction_id := vars["transactionID"]
 
-	//fmt.Println(wallet_id)
+	//get transaction from database
+	transaction := models.Transaction{}
+	ae.Storage.DB.Where("transactions.id = ?", transaction_id).First(&transaction)
+
+	//get account from database
+	account := models.Account{}
+	ae.Storage.DB.Where("accounts.id = ?", transaction.AccountID).First(&account)
+
+	//get transaction category from database
+	category := models.Category{}
+	ae.Storage.DB.Where("categories.id = ?", transaction.CategoryID).First(&category)
+
+	println("########### transaction type: " + category.TransactionType + "###############")
+
+	//update account balance
+	if category.TransactionType == "expense" {
+		//do reverse action, we are 'undoing' the change because we are deleting transaction
+		account.Amount = account.Amount + transaction.Amount
+	} else if category.TransactionType == "income" {
+		//do reverse action, we are 'undoing' the change because we are deleting transaction
+		account.Amount = account.Amount - transaction.Amount
+	}
+
+	//save account change to db
+	ae.Storage.DB.Save(&account)
 
 	//delete transaction
 	ae.Storage.DB.Delete(&models.Transaction{}, transaction_id)
