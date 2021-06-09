d49a80b4f5a700b0593a8c872de6f313bcbf7654
Theo Riandy (s3827202) <76926710+s3827202@users.noreply.github.com>
2021-06-09 15:06:18 +1000

    temp

--- controllers/transaction.go
+++ controllers/transaction.go
@@ -42,6 +42,7 @@ func (ae AppEngine) CreateExpenseTrx(w http.ResponseWriter, r *http.Request) {
 	amount := 0.0
 	error_message := ""
 	error_message_bool := false
+	user_id := session.Values["user"].(models.User).ID
 
 	//get
 	db.Where("user_id = ?", session.Values["user"].(models.User).ID).Find(&accounts)
@@ -54,103 +55,103 @@ func (ae AppEngine) CreateExpenseTrx(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println(err)
 		}
-		defer file.Close()
 
-		size := fileheader.Size
+		//image uploader
+		if file != nil {
+			defer file.Close()
 
-		r.ParseMultipartForm(size)
+			size := fileheader.Size
 
-		buffer := make([]byte, size)
-		file.Read(buffer)
+			r.ParseMultipartForm(size)
 
-		fileBytes := bytes.NewReader(buffer)
-		fileType := http.DetectContentType(buffer)
+			buffer := make([]byte, size)
+			file.Read(buffer)
 
-		if fileType != "image/jpeg" {
-			error_message = "File uploaded is not an image. Please upload an image. ([filename].jpg)"
-			error_message_bool = true
-		}
+			fileBytes := bytes.NewReader(buffer)
+			fileType := http.DetectContentType(buffer)
 
-		if !error_message_bool {
-			_, err = ae.S3Client.Upload(&s3manager.UploadInput{
-				Bucket: aws.String(config.AWS_BUCKET_NAME),
-				Key:    aws.String(username + "_" + date.Format("2006-01-02 15:04:05") + ".jpg"),
-				Body:   fileBytes,
-				ACL:    aws.String("public-read"),
-			})
-
-			if err != nil {
-				fmt.Println(err)
+			if fileType != "image/jpeg" {
+				error_message = "File uploaded is not an image. Please upload an image. ([filename].jpg)"
+				error_message_bool = true
+			} else if fileType != "image/png" {
+				error_message = "File uploaded is not an image. Please upload an image. ([filename].png)"
+				error_message_bool = true
 			}
-			//get category id using category name and user_id
-			user_id := session.Values["user"].(models.User).ID
-			categoryName := r.FormValue("category")
 
-			category := -1
-			//ae.Storage.DB.Select("categories.ID").Where("categories.name = ? and categories.user_id = ", categoryName, user_id).First(&category)
-			ae.Storage.DB.Raw("SELECT id FROM categories WHERE name = ? and user_id = ?", categoryName, user_id).Scan(&category)
+			if !error_message_bool {
+				_, err = ae.S3Client.Upload(&s3manager.UploadInput{
+					Bucket: aws.String(config.AWS_BUCKET_NAME),
+					Key:    aws.String(username + "_" + date.Format("2006-01-02 15:04:05") + ".jpg"),
+					Body:   fileBytes,
+					ACL:    aws.String("public-read"),
+				})
 
-			//if category doesn't exist create it
-			if category < 0{
-				//create category
-				newCategory := models.Category{
-					Name: categoryName,
-					TransactionType: "expense",
-					UserID:     session.Values["user"].(models.User).ID,
+				if err != nil {
+					fmt.Println(err)
 				}
-	
-				db.Create(&newCategory)
-
-				//get id of new category
-				ae.Storage.DB.Raw("SELECT id FROM categories WHERE name = ?", categoryName).Scan(&category)
 			}
-
-			//save transaction
-
-			account_id, _ = strconv.Atoi(r.FormValue("account"))
-			category_id = category
-			//category_id, _ = strconv.Atoi(r.FormValue("category"))
-			amount, _ = strconv.ParseFloat(r.FormValue("amount"), 64)
-
-			trx := models.Transaction{
-				Date:       date,
-				AccountID:  uint(account_id),
-				CategoryID: uint(category_id),
-				UserID:     session.Values["user"].(models.User).ID,
-				Amount:     amount,
-				Note:       r.FormValue("notes"),
-				ImgURL:     config.AWS_IMG_PATH + username + "_" + date.Format("2006-01-02") + "+" + date.Format("15") + "%3A" + date.Format("04") + "%3A" + date.Format("05") + ".jpg",
-			}
-
-			fmt.Println(category_id)
-
-			db.Create(&trx)
-
-			//update account balance
-			//get account from database
-			account := models.Account{}
-			ae.Storage.DB.Where("accounts.id = ?", account_id).First(&account)
-
-			//get transaction category from database
-			transactionCategory := models.Category{}
-			ae.Storage.DB.Where("categories.id = ?", category_id).First(&transactionCategory)
-
-			//update account balance
-			if transactionCategory.TransactionType == "expense" {
-				account.Amount = account.Amount - amount
-			} else if transactionCategory.TransactionType == "income" {
-				account.Amount = account.Amount + amount
-			}
-
-			//save account change to db
-			ae.Storage.DB.Save(&account)
-
-			//send user back to wallet page
-			url := "/wallet/" + strconv.Itoa(account_id)
-
-			http.Redirect(w, r, url, http.StatusFound)
-			return
 		}
+
+		//get category id using category name and user_id
+		categoryName := r.FormValue("category")
+
+		categoryID := -1
+		//ae.Storage.DB.Select("categories.ID").Where("categories.name = ? and categories.user_id = ", categoryName, user_id).First(&category)
+		ae.Storage.DB.Raw("SELECT id FROM categories WHERE name = ? and user_id = ?", categoryName, user_id).Scan(&categoryID)
+
+		//if category doesn't exist create it
+		if categoryID < 0 {
+			//create category
+			newCategory := models.Category{
+				Name:            categoryName,
+				TransactionType: "expense",
+				UserID:          session.Values["user"].(models.User).ID,
+			}
+
+			db.Create(&newCategory)
+			categoryID = int(newCategory.ID)
+		}
+
+		//save transaction
+		account_id, _ = strconv.Atoi(r.FormValue("account"))
+		amount, _ = strconv.ParseFloat(r.FormValue("amount"), 64)
+
+		trx := models.Transaction{
+			Date:       date,
+			AccountID:  uint(account_id),
+			CategoryID: uint(category_id),
+			UserID:     session.Values["user"].(models.User).ID,
+			Amount:     amount,
+			Note:       r.FormValue("notes"),
+			ImgURL:     config.AWS_IMG_PATH + username + "_" + date.Format("2006-01-02") + "+" + date.Format("15") + "%3A" + date.Format("04") + "%3A" + date.Format("05") + ".jpg",
+		}
+		db.Create(&trx)
+
+		//update account balance
+		//get account from database
+		account := models.Account{}
+		ae.Storage.DB.Where("id = ?", account_id).First(&account)
+
+		//get transaction category from database
+		transactionCategory := models.Category{}
+		ae.Storage.DB.Where("id = ?", category_id).First(&transactionCategory)
+
+		//update account balance
+		if transactionCategory.TransactionType == "expense" {
+			account.Amount = account.Amount - amount
+		} else if transactionCategory.TransactionType == "income" {
+			account.Amount = account.Amount + amount
+		}
+
+		//save account change to db
+		ae.Storage.DB.Save(&account)
+
+		//send user back to wallet page
+		url := "/wallet/" + strconv.Itoa(account_id)
+
+		http.Redirect(w, r, url, http.StatusFound)
+		return
+
 	}
 
 	t, _ := template.ParseFiles(viewPage, config.HEADER_PATH, config.NAVIGATION_PATH)
@@ -246,20 +247,19 @@ func (ae AppEngine) CreateIncomeTrx(w http.ResponseWriter, r *http.Request) {
 			ae.Storage.DB.Raw("SELECT id FROM categories WHERE name = ? and user_id = ?", categoryName, user_id).Scan(&category)
 
 			//if category doesn't exist create it
-			if category < 0{
+			if category < 0 {
 				//create category
 				newCategory := models.Category{
-					Name: categoryName,
+					Name:            categoryName,
 					TransactionType: "income",
-					UserID:     session.Values["user"].(models.User).ID,
+					UserID:          session.Values["user"].(models.User).ID,
 				}
-	
+
 				db.Create(&newCategory)
 
 				//get id of new category
 				ae.Storage.DB.Raw("SELECT id FROM categories WHERE name = ?", categoryName).Scan(&category)
 			}
-
 
 			//save transaction
 
@@ -336,22 +336,21 @@ func (ae AppEngine) Transaction(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 	}
 
 	//get transaction id from url
 	vars := mux.Vars(r)
-    transaction_id := vars["ID"]
+	transaction_id := vars["ID"]
 
 	//get transaction data from database
 	transactionData := models.Transaction{}
 	ae.Storage.DB.Where("transactions.id = ?", transaction_id).First(&transactionData)
-	
-	//get transaction category 
+
+	//get transaction category
 	category := models.Category{}
 	ae.Storage.DB.Where("categories.id = ?", transactionData.CategoryID).First(&category)
-
 
 	viewPage := "views/transaction.html"
 	assetsUrl := "http://" + r.Host + "/assets/"
@@ -362,14 +361,13 @@ func (ae AppEngine) Transaction(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles(viewPage, config.HEADER_PATH, config.NAVIGATION_PATH)
 
 	data := map[string]interface{}{
-		"title":    "Transaction",
-		"assets":   assetsUrl,
-		"username": username,
+		"title":           "Transaction",
+		"assets":          assetsUrl,
+		"username":        username,
 		"transactionData": transactionData,
-		"category": category,
+		"category":        category,
 	}
 
 	w.WriteHeader(http.StatusOK)
 	t.ExecuteTemplate(w, "transaction", data)
 }
-
